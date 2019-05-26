32815f363292b5dffc0d6a9f8a1aa77bd935197a
Nana Adjei Manu <claeusdev@outlook.com>
2019-05-26 05:09:03 +0000

    user api endpoint

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,8 @@
+class UserResource < JSONAPI::Resource
+ attributes :email, :username, :password, :password_confirmation
+
+
+ def fetchable_fields
+  super - [:password, :password_confirmation]
+ end
+end
